d4adebdc8c10339edf1a8c451f16d53e0f0ab200
bhushhhhann <165308129+bhushhhhann@users.noreply.github.com>
2025-12-04 00:00:06 +0530

    Add files via upload

---
+++ Basics/GenerateParentheses.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        ans("",null,0,0,3);
+    }
+    public static List<String> gn(int n){
+        List<String>ll=new ArrayList<String>();
+        ans("",ll,0,0,n);
+        return ll;
+    }
+    public static void ans(String ans ,List<String>ll,int open ,int close ,int n){
+        if(ans.length()==n*2){
+            ll.add(ans);
+            return;
+        }
+        if(open<n){
+            ans(ans+"(",ll,open+1,close,n);
+        }
+        if(close<open){
+            ans(ans+")",ll,open ,close+1,n);
+        }
+    }
+}
